8727b4d357de8e018299b18f241d15aa58e7a949
marcosdavia89 <marcosdavia6@gmail.com>
2026-08-07 17:16:22 -0300

    Add files via upload

---
+++ uc15-main/marcos2/script.js
@@ -0,0 +1,39 @@
+let curso = 'medtec'
+let ano = 2;
+let nome = 'marcos'
+
+console.log("Bem vindo! " + nome + "Você é do curso " + curso
+)
+
+/*let texto = "Bem vindo! " + nome + ''você é do curso'' + curso + "da série do" + amo + "o" */
+
+/*let texto = "bem vindo" ${nome}! você é do ${curso} e da série $(ano) */
+
+console.log(texto)
+
+let  multilinha = `
+primeira linha
+segunda linha
+terceira linha
+`
+console.log(multilinhas)
+
+let texto = "desenvolvedor front-end"
+
+// comprimento 
+let tamanho = tamanho.lenght
+
+// remover espaço das bordas 
+
+
+texto.trim()
+
+//maiúsculo e minúsculo
+
+texto.trim().toUpperCase()
+texto.trim().toUpperCase()
+
+let tamanho = texto.replace(`front-end`, `back-end`)
+
+console.log(mudança)
+console.log(tamanho)
