9f709f9d6c82afd883af870fc7b8f2f6483551b3
marcosdavia89 <marcosdavia6@gmail.com>
2026-08-20 17:53:23 -0300

    Add files via upload

---
+++ atividade-20.08.26/script.js
@@ -0,0 +1,8 @@
+// 1.
+let descricao = temperatura > 30 ? 'Quente' : 'Agradável';
+
+// 2.
+let tipo = numero % 2 === 0 ? 'par' : 'ímpar';
+
+// 3.
+let saudacao = hora < 12 ? 'Bom dia' : 'Boa tarde/noite';
